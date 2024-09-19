8fe3b5dfe66be051cf4bd22c06aeb27246086834
alexandrolastra <alexandrolastra.fr@gmail.com>
2024-09-19 20:49:42 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,32 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import ErrorPage from "./pages/ErrorPage";
 
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <div className="App relative z-20 pt-20">
+    <div className="App relanpm tive z-20 pt-20">
       <Navbar />
 
+      {/*   Add <Route /> components between <Routes> and </Routes>   */}
+      {
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/students/:studentId" element={<StudentDetailsPage />} />
+          <Route path="/profile" element={<UserProfilePage />} />
+
+          <Route path="*" element={<h1>404 Page</h1>} />
+        </Routes>
+      }
       <div className="pages">
-        <HomePage />
+        {/*  <HomePage />
 
         <StudentDetailsPage />
 
-        <UserProfilePage />
+        <UserProfilePage /> */}
       </div>
-
     </div>
   );
 }
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,19 @@
+<main>
+  <div>
+    <p>Error Page</p>;
+  </div>
+</main>;
+
+function ErrorPage() {
+
+  return (
+    <div className="border-2 border-rose-500 m-2">
+      <h1> ERROR PAGE</h1>
+      <div className="flex justify-between items-center p-2 font-bold border-b w-full">
+        <span>ERROR PAGE</span>
+      </div>
+    </div>
+  );
+}
+
+export default ErrorPage;
